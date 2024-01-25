34f416180484566f3d80bcb16cbf9c74c7265d38
ankur0904 <ankursingh91002@gmail.com>
2024-01-25 12:52:56 +0530

    Improves app & UI

--- app.py
+++ app.py
@@ -1,61 +1,53 @@
 from flask import Flask, render_template, request, redirect
-# from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy
-from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-db=sqlalchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+app.app_context().push()
 class Todo(db.Model):
-    sno=db.Column(db.Integer, primary_key=True)
-    title=db.Column(db.String(200), nullable=False)
-    desc=db.Column(db.String(500), nullable=False)
-    date_created=db.Column(db.DateTime, default=datetime.utcnow)
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
 
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
-@app.route('/', method=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    if request.method=="POST":
-        title=request.form['title']
-        desc=request.form['desc']
-        todo = Todo(title= title, desc=desc)
+    if request.method == "POST":
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc)
         db.session.add(todo)
         db.session.commit()
-    allTodo=Todo.query.all()
-    return render_template ('index.html', allTodo=allTodo)
+    allTodo = Todo.query.all()
+    return render_template('index.html', allTodo=allTodo)
 
-
-
-# @app.route('/update'/<int:sno>, method=['GET','POST'])
-@app.route('/update', method=['GET','POST'])
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
 def update(sno):
-    if request.method=='POST':
-        title=request.form['title']
-        desc=request.form['desc']
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
         todo = Todo.query.filter_by(sno=sno).first()
-        todo.title=title
-        todo.desc=desc
-        db.session.add(todo)
+        todo.title = title
+        todo.desc = desc
         db.session.commit()
         return redirect("/")
-
+    
     todo = Todo.query.filter_by(sno=sno).first()
-    return render_template ('update.html', todo=todo)
+    return render_template('update.html', todo=todo)
 
-# @app.route('/delete'/<int:sno>)
-@app.route('/delete')
+@app.route('/delete/<int:sno>')
 def delete(sno):
     todo = Todo.query.filter_by(sno=sno).first()
     db.session.delete(todo)
     db.session.commit()
     return redirect("/")
 
-#for multiple end points above route is one end point and the products id other end point
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True, port=8000)
+
